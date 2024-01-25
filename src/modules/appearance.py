54bdac6600a032fee3c76c339afa2fa30dd69376
XtremeTHN <nigthmaresans2@gmail.com>
2024-01-24 19:57:54 -0600

    I'll remove the color scheme section

--- src/modules/appearance.py
+++ src/modules/appearance.py
@@ -9,12 +9,12 @@ def ColorScheme(color_scheme_name: str, subtitle: str, img_name: str, callback):
 
     box = VBox(spacing=0)
     
-    box_img = Gtk.Image.new_from_resource(f'/com/github/XtremeTHN/ControlCenter/src/res/uncompiled/{img_name}.png')
-    box_img.set_pixel_size(214)
-    box_img.add_css_class('colorscheme-img')
+    img = Gtk.Image.new_from_resource(f'/com/github/XtremeTHN/ControlCenter/src/res/uncompiled/{img_name}.png')
+    img.set_pixel_size(168)
+    img.add_css_class('colorscheme-img')
     box_foot_text = Gtk.Label.new(subtitle)
     
-    box.appends(box_img, box_foot_text)
+    box.appends(img, box_foot_text)
     border_box.set_child(box)
 
     border_box.connect('toggled', callback, color_scheme_name)
@@ -27,14 +27,19 @@ class AppearancePage(VBox):
         # AppearanceConfig.__init__(self)
         super().__init__(spacing=10)
         
-        self.color_scheme_box = VBox(spacing=0)
-        self.color_scheme_box_title = Gtk.Label(label='<span weight="bold">Style</span>', use_markup=True, halign=Gtk.Align.START)
-        self.color_scheme_box_color_schemes = HBox(spacing=20)
-        set_margins(self.color_scheme_box_color_schemes, [10])
+        color_schemes_group = Adw.PreferencesGroup()
+        color_schemes_group.set_title("Style")
+        color_schemes_group.set_description("Choose a color scheme")
 
-        self.color_scheme_box_color_schemes.appends(ColorScheme("Dark", "dark", "dark-color-scheme", self.color_callback))
-        self.color_scheme_box.appends(self.color_scheme_box_title, self.color_scheme_box_color_schemes)
-        self.append(self.color_scheme_box)
+        cs_buttons = HBox(homogeneous=True)
+        
+        light = ColorScheme("light", "Light", "light-color-scheme", self.color_callback)
+        dark = ColorScheme("dark", "Dark", "dark-color-scheme", self.color_callback)
+
+        cs_buttons.appends(light, dark)
+
+        color_schemes_group.add(cs_buttons)
+        self.append(color_schemes_group)
 
     def color_callback(self, widget, color_scheme_name):
         if widget.get_active():
